e2b503a717a14602b6250dbb1721a4d6b29e6814
PandaBoo <thuyenle1307@gmail.com>
2025-11-16 13:43:33 +0700

    1611

--- watch-be/server.js
+++ watch-be/server.js
@@ -34,13 +34,14 @@ const historyRouter = require("./src/web/routers/historyRouter");
 const UserTrendProfile = require("./src/web/routers/userTrendProfileRoute");
 const recommendation = require("./src/web/routers/userRecommendationRoute");
 const emotion = require("./src/web/routers/emotionRoutes");
-
+const otpRouter = require("./src/web/routers/otpRouter");
 const playlistSong = require("./src/web/routers/playlistSongRoute");
 
 // 🛠️ Dùng routes
 app.use("/api/users", userRouter);
 app.use("/api/albums", albumRoutes);
 app.use("/api/albums", albumSongRouter);
+app.use("/api/users", otpRouter);
 
 app.use("/api/singers", singerRoutes);
 app.use("/api/genres", genreRoutes);
